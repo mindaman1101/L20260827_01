dd1249962c5bbe298c9bec27c7a76de8d8a127bf
mindaman1101 <mindaman1101@ajou.ac.kr>
2026-08-27 09:44:40 +0900

    동적 할당 추가

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include "Player.h"
+#include "Goblin.h"
+#include "Slime.h"
+#include "WildBoar.h"
 
 using namespace std;
 
 int main()
 {
+	APlayer* Player = new APlayer();
+	AGoblin* Goblin = new AGoblin();
+	ASlime* Slime = new ASlime();
+	AWildBoar* WildBoar = new AWildBoar();
+
+	delete Player;
+	delete Goblin;
+	delete Slime;
+	delete WildBoar;	
+	
 	return 0;
 }
